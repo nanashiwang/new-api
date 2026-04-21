4674b39d4505e15d8aa33d593501265dec918a77
n33 <1741170479@qq.com>
2026-04-22 00:08:10 +0800

    fix(profit-board): 移除 Tab 栏永久下划线
    通过 CSS 覆盖 Semi UI .semi-tabs-bar 的 border-bottom，
    使收益看板 Tab 下划线仅在激活或 hover 时显示

--- web/dist/assets/classDiagram-v2-COTLJTTW-JQvY_xam.js
+++
@@ -1,1 +0,0 @@
-import{c as a,C as s,a as e,s as t}from"./chunk-A2AXSNBT-Cxx3LnNj.js";import{_ as i}from"./mermaid-BEOrcoab.js";import"./chunk-RZ5BOZE2-Dm9kVYz2.js";import"./react-core-CZZu3mg-.js";var _={parser:a,get db(){return new s},renderer:e,styles:t,init:i(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{_ as diagram};
